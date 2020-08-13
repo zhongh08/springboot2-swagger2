778040b652fb34c2d3a9d09e989fd0576b1733d3
zhongh08 <zhongh08@163.com>
2020-08-13 19:22:36 +0800

    springboot2-swagger2第一次提交代码

---
+++ src/test/java/com/syoki/springboot2swagger2/Springboot2Swagger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syoki.springboot2swagger2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2Swagger2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
